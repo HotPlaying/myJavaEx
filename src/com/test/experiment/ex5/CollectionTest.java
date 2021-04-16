9705d422ac2db9b55f44d9cab6900b4b644bead9
Hotplaying <tangrd98>
2021-04-17 00:18:42 +0800

    update

--- src/com/test/experiment/ex5/CollectionTest.java
+++ src/com/test/experiment/ex5/CollectionTest.java
@@ -1,9 +1,12 @@
 package com.test.experiment.ex5;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CollectionTest {
     public static void main(String[] args) {
+        Map<String, Object> map = new ConcurrentHashMap<>();
+
         System.out.println("输入要转换的字符串：");
         while (true) {
             Scanner scanner = new Scanner(System.in);
--- src/com/test/utilforwork/FileRenameBatch.java
+++ src/com/test/utilforwork/FileRenameBatch.java
@@ -2,6 +2,7 @@ package com.test.utilforwork;
 
 import org.junit.Test;
 
+import javax.swing.text.html.Option;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
@@ -13,14 +14,10 @@ import java.util.Scanner;
  * @description
  */
 public class FileRenameBatch {
+    public static String subtitleFile_type = "srt";
+    public static int step = 0;
+
     public static void main(String[] args) {
-
-        /**
-         * D:\Video\Animation\_字幕\[异域-11番小队]零之使魔3三美姬的轮舞Zero_no_Tsukaima_Princesse_no_Rondoi][HKG]
-         * D:\Video\Animation\[VCB-Studio] Zero no Tsukaima\[VCB-Studio] Zero no Tsukaima Princess no Rondo [Ma10p_1080p]\SPs
-         * 1
-         */
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("输入字幕文件目录路径：");
         String dstPath = scanner.nextLine();
@@ -31,72 +28,70 @@ public class FileRenameBatch {
         System.out.println("请选择：1.验证，2.真正改名");
         String option = scanner.nextLine();
 
+        step = 1;
+        fileRenameBatch(srcPath, dstPath, option);
+        if (!"1".equals(option)) return;
+
+
+        System.out.println("是否真正改名：1.是，2.否");
+        option = scanner.nextLine();
+        if (!"1".equals(option)) return;
+
+        step = 2;
+        fileRenameBatch(srcPath, dstPath, option);
+    }
+
+    private static void fileRenameBatch(String srcPath, String dstPath, String option) {
         File dst = new File(dstPath);
         File src = new File(srcPath);
         assert dst.isDirectory();
         assert src.isDirectory();
         List<String> srcFileList = Arrays.asList(src.list());
         List<String> dstFileList = Arrays.asList(dst.list());
-
         int i = 0;
-
-        for (String s : srcFileList) {
-            if (!s.matches(".*\\.mkv")) continue;
-            else {
-                while (!dstFileList.get(i).matches(".*\\.ass")) i++;
-                String fileNewName = s.split("\\.")[0] + ".ass";
-//                String n = i < 10 ? "0" + i : Integer.toString(i);
-                File item = new File(dst.getPath() + "\\" + dstFileList.get(i++));
-                File item_new = new File(dst.getPath() + "\\" + fileNewName);
-
-                if ("1".equals(option)) {
-                    System.out.printf("\"%s\" renameTo \"%s\" \n", item.getName(), item_new.getName());
-                } else if ("2".equals(option)) {
-                    System.out.printf("\"%s\" renameTo \"%s\" is [%s]\n", item.getName(), item_new.getName(), item.renameTo(item_new));
-                } else {
-                    System.out.println("无效选项");
-                }
-            }
-        }
-        if (!"1".equals(option)) return;
-        System.out.println("是否真正改名：1.是，2.否");
-        option = scanner.nextLine();
-        i = 0;
-        if (!"1".equals(option)) return;
         boolean res = true;
         for (String s : srcFileList) {
             if (!s.matches(".*\\.mkv")) continue;
-            else {
-                while (!dstFileList.get(i).matches(".*\\.ass")) i++;
-                String fileNewName = s.split("\\.")[0] + ".ass";
+            while (!dstFileList.get(i).matches(".*\\." + subtitleFile_type)) i++;
+
+            String fileNewName = s.substring(0, s.lastIndexOf('.')) + "." + subtitleFile_type;
 //                String n = i < 10 ? "0" + i : Integer.toString(i);
-                File item = new File(dst.getPath() + "\\" + dstFileList.get(i++));
-                File item_new = new File(dst.getPath() + "\\" + fileNewName);
-                res = res && item.renameTo(item_new);
+            File item = new File(dst.getPath() + "\\" + dstFileList.get(i++));
+            File item_new = new File(dst.getPath() + "\\" + fileNewName);
+
+
+            if (step == 1) {
+                switch (option) {
+                    case "1":
+                        System.out.printf("\"%s\" renameTo \"%s\" \n", item.getName(), item_new.getName());
+                        break;
+                    case "2":
+                        System.out.printf("\"%s\" renameTo \"%s\" is [%s]\n", item.getName(), item_new.getName(), item.renameTo(item_new));
+                        break;
+                }
+            }
+            if (step == 2) {
+                switch (option) {
+                    case "1":
+                        res = res && item.renameTo(item_new);
+                        System.out.print(res ? "-" : "|");
+                        break;
+                    case "2":
+                        break;
+                }
             }
         }
-        System.out.println("批量重命名" + (res ? "成功" : "失败"));
-//        String dstName = "\\[VCB-Studio] Zero no Tsukaima [#i#][Ma10p_1080p][x265_flac].ass";
-
-//        int i = 0;
-//        for (String s : dstFileList) {
-//            if (s.matches(".*\\.ass")) {
-//                String n = i < 10 ? "0" + i : Integer.toString(i);
-//                i++;
-//                String item_newName = dstName.replaceAll("#i#", n);
-//                File item = new File(dst.getPath() + "\\" + s);
-//                File item_new = new File(dst.getPath() + item_newName);
-//                System.out.println("[" + item.getName() + "] renameTo [" + item_new.getName() + "] is [" + item.renameTo(item_new) + "]");
-//            }
-//        }
+        if (2 == step && "1".equals(option)) {
+            System.out.println("\n批量重命名" + (res ? "成功" : "失败"));
+        }
     }
 
     @Test
-    public void test(){
+    public void test() {
         File file = new File("F:\\DEV Files\\Tomcat\\apache-tomcat-8.5.64\\webapps\\comics\\我立于百万生命之上");
-        String [] fileList = file.list();
-        for(String s : fileList){
-            if (s.matches(".*\\.html")){
+        String[] fileList = file.list();
+        for (String s : fileList) {
+            if (s.matches(".*\\.html")) {
                 System.out.println(s);
 
             }
