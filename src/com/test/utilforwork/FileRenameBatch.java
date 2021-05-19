c4654572d433f2896c8f5957b83944f55633614e
Hotplaying <tangrd98>
2021-05-20 01:38:25 +0800

    update:FileRenameBatch.java

--- src/com/test/utilforwork/FileRenameBatch.java
+++ src/com/test/utilforwork/FileRenameBatch.java
@@ -2,7 +2,6 @@ package com.test.utilforwork;
 
 import org.junit.Test;
 
-import javax.swing.text.html.Option;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
@@ -17,36 +16,14 @@ public class FileRenameBatch {
     public static String subtitleFile_type = "ass";
     public static int step = 0;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("输入字幕文件目录路径：");
-        String dstPath = scanner.nextLine();
-        System.out.println(dstPath);
-        System.out.println("输入视频文件目录路径：");
-        String srcPath = scanner.nextLine();
-        System.out.println(srcPath);
-        System.out.println("请选择：1.验证，2.真正改名");
-        String option = scanner.nextLine();
-
-        step = 1;
-        fileRenameBatch(srcPath, dstPath, option);
-        if (!"1".equals(option)) return;
-
-
-        System.out.println("是否真正改名：1.是，2.否");
-        option = scanner.nextLine();
-        if (!"1".equals(option)) return;
-
-        step = 2;
-        fileRenameBatch(srcPath, dstPath, option);
-    }
+    public static int times = 100;
 
     private static void fileRenameBatch(String srcPath, String dstPath, String option) {
         File dst = new File(dstPath);
         File src = new File(srcPath);
         assert dst.isDirectory();
         assert src.isDirectory();
-        List<String> srcFileList = Arrays.asList(src.list());
+        String[] srcFileList = src.list();
         List<String> dstFileList = Arrays.asList(dst.list());
         int i = 0;
         boolean res = true;
@@ -84,6 +61,104 @@ public class FileRenameBatch {
         if (2 == step && "1".equals(option)) {
             System.out.println("\n批量重命名" + (res ? "成功" : "失败"));
         }
+    }
+
+    private static void fileRenameBatchOnce(String dstPath, String option) {
+        File dst = new File(dstPath);
+
+        assert dst.isDirectory();
+
+        String[] dstFileList = dst.list();
+
+        boolean res = true;
+
+        for (String s : dstFileList) {
+//            System.out.println(s);
+            if (!s.matches("\\d+_.*")) continue;
+
+            String[] splits = s.split("_");
+
+            String fileNewName = s.split("_")[1];
+            File item = new File(dst.getPath() + File.separator + s);
+            File item_new = new File(dst.getPath() + File.separator + fileNewName);
+
+            switch (option) {
+                case "1":
+                    System.out.println(s);
+                    break;
+                case "2":
+                    System.out.printf("【%s】 -> 【%s】 \n", item.getName(), item_new.getName());
+                    break;
+                case "3":
+                    res = res && item.renameTo(item_new);
+                    System.out.print(res ? "-" : "|");
+//                    System.out.printf("\"%s\" renameTo \"%s\" is [%s]\n", item.getName(), item_new.getName(), item.renameTo(item_new));
+                    break;
+            }
+        }
+        if ("3".equals(option)) {
+            System.out.println("\n批量重命名" + (res ? "成功" : "失败"));
+        }
+    }
+
+    /**
+     * 批量按照动画BD视频文件名来重命名字幕文件 - 流程
+     */
+    public static void videoSubtitleFileRename() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入字幕文件目录路径：");
+        String dstPath = scanner.nextLine();
+        System.out.println(dstPath);
+        System.out.println("输入视频文件目录路径：");
+        String srcPath = scanner.nextLine();
+        System.out.println(srcPath);
+        System.out.println("请选择：1.验证，2.真正改名");
+        String option = scanner.nextLine();
+
+        step = 1;
+        fileRenameBatch(srcPath, dstPath, option);
+        if (!"1".equals(option)) return;
+
+
+        System.out.println("是否真正改名：1.是，2.否");
+        option = scanner.nextLine();
+        if (!"1".equals(option)) return;
+
+        step = 2;
+        fileRenameBatch(srcPath, dstPath, option);
+    }
+
+    public static void main(String[] args) {
+        numMusicFileRename();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("选择操作：1.动画BD字幕重命名，2.音乐文件重命名");
+//        String option = scanner.nextLine();
+//        switch (option) {
+//            case "1":
+//                videoSubtitleFileRename();
+//                break;
+//            case "2":
+//                numMusicFileRename();
+//        }
+    }
+
+    /**
+     * 音乐重命名去掉数字前缀 - 流程
+     */
+//    @Test
+    public static void numMusicFileRename() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入音乐文件目录路径：");
+        String dstPath = scanner.nextLine();
+        System.out.println(dstPath);
+        while (true) {
+            System.out.println("请选择：1.列出符合条件的文件，2.预览改名，3.真正改名，q.退出");
+            String option = scanner.nextLine();
+            if ("q".equals(option)) return;
+            fileRenameBatchOnce(dstPath, option);
+        }
+
     }
 
     @Test
