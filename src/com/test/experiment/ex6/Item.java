bc7e348e56ad9110c9ff92c17c0ddc352cc6d84a
trd <854397717@qq.com>
2021-07-15 20:24:40 +0800

    Add DP, Update dependencies

---
+++ src/com/test/experiment/ex6/Item.java
@@ -0,0 +1,13 @@
+package com.test.experiment.ex6;
+
+import lombok.Data;
+
+/**
+ * @author tangrd
+ * @date 2021/7/14 21:57
+ */
+@Data
+public class Item {
+    public Integer weight;
+    public Integer value;
+}
