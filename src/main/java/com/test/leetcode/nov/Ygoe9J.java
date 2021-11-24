396c6db1560ea912748c83a7b2b79104d3e4c8d0
hotplaying <tangrd98@outlook.com>
2021-11-25 00:56:02 +0800

    leetcode: Ygoe9J

---
+++ src/main/java/com/test/leetcode/nov/Ygoe9J.java
@@ -0,0 +1,73 @@
+package com.test.leetcode.nov;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tangrd
+ * @since 2021/11/24 22:43
+ * https://leetcode-cn.com/problems/Ygoe9J/
+ */
+public class Ygoe9J {
+    @Test
+    public void test() {
+        int[] can = {2, 3, 6, 7};
+        int target = 7;
+        System.out.println(s1(can, target));
+    }
+
+    public List<List<Integer>> s1(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> com = new ArrayList<>();
+        dfs(candidates, target, ans, com, 0);
+        return ans;
+    }
+
+    public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> com, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<>(com));
+            return;
+        }
+
+        // 直接跳过
+        dfs(candidates, target, ans, com, idx + 1);
+
+        // 选择当前数
+        int current = candidates[idx];
+        if (target - current >= 0) {
+            com.add(current);
+            dfs(candidates, target - current, ans, com, idx);
+            com.remove(com.size() - 1);
+        }
+
+//
+//        for (int a : candidates) {
+//            list.add(a);
+//            int sum = sum(list);
+//            if (sum < target) {
+//                List<Integer> b = dfs(candidates, target - a, ans, times);
+//                if (null == b) {
+//                    return null;
+//                }
+//                list.addAll(b);
+//            } else if (sum == target) {
+//                return list;
+//            } else {
+//                list.remove(list.size() - 1);
+//            }
+//        }
+    }
+
+    int sum(List<Integer> list) {
+        int a = 0;
+        for (Integer integer : list) {
+            a += integer;
+        }
+        return a;
+    }
+}
