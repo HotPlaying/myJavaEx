3bdfc06c4687d490ce55ab7081bb3be073db6624
hotplaying <tangrd98@outlook.com>
2021-11-24 22:52:09 +0800

    StringToUrlLcci

--- src/main/java/com/test/leetcode/nov/StringToUrlLcci.java
+++ src/main/java/com/test/leetcode/nov/StringToUrlLcci.java
@@ -1,5 +1,7 @@
 package com.test.leetcode.nov;
 
+import org.junit.Test;
+
 /**
  * @author trd
  * @since 2021/11/23 15:44
@@ -7,8 +9,40 @@ package com.test.leetcode.nov;
  */
 public class StringToUrlLcci {
     public String solution(String s, int length) {
-        char[] chars = s.toCharArray();
-        return s;
+        int slen = s.length();
+        int kgs = (slen - length) / 2;
+
+//        for (int i = 0; i < length; i++) {
+//            char charAt = s.charAt(i);
+//            if (charAt == ' ') {
+//
+//            }
+//        }
+
+        char[] ans = new char[slen];
+        int i = 0, j = 0;
+        while (j < length) {
+            char charAt = s.charAt(j);
+            if (charAt == ' ' && kgs > 0) {
+                ans[i] = '%';
+                ans[i + 1] = '2';
+                ans[i + 2] = '0';
+                kgs--;
+                i += 3;
+            } else {
+                ans[i] = charAt;
+                i++;
+            }
+            j++;
+        }
+
+        return String.valueOf(ans,0, i);
     }
 
+    @Test
+    public void test() {
+        String s = solution("ds sdfs afs sdfa dfssf asdf             ",
+                27);
+        System.out.println(s);
+    }
 }
