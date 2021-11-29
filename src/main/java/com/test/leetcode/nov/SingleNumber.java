42cd835c2313fa6d6609024570846ed081156388
trd <854397717@qq.com>
2021-11-29 17:59:30 +0800

    leetcode: SingleNumber

---
+++ src/main/java/com/test/leetcode/nov/SingleNumber.java
@@ -0,0 +1,13 @@
+package com.test.leetcode.nov;
+
+/**
+ * @author trd
+ * @since 2021/11/29 17:58
+ * https://leetcode-cn.com/problems/WGki4K/
+ */
+public class SingleNumber {
+    public int solution(int[] nums) {
+
+        return 1;
+    }
+}
