d1a184967ee6f10bc7d606576cd3f245c99b2835
hotplaying <tangrd98@outlook.com>
2021-12-05 16:52:16 +0800

    leetcode: BinarySearch Non-recursive

--- src/main/java/com/test/leetcode/dec/BinarySearch.java
+++ src/main/java/com/test/leetcode/dec/BinarySearch.java
@@ -16,7 +16,7 @@ public class BinarySearch {
         System.out.println("预期结果：");
         System.out.println(answer.trueAns);
         System.out.println("执行结果：");
-        System.out.println(search(answer.arr, answer.target));
+        System.out.println(search2(answer.arr, answer.target));
     });
 
     @Test
@@ -25,6 +25,8 @@ public class BinarySearch {
         FUN.accept(new Answer(arr, 9, 4));
         int[] arr1 = {5};
         FUN.accept(new Answer(arr1, 5, 0));
+        int[] arr2 = {-1, 0, 3, 5, 9, 12};
+        FUN.accept(new Answer(arr2, 2, -1));
     }
 
     public int search(int[] nums, int target) {
@@ -43,6 +45,23 @@ public class BinarySearch {
             return search(nums, target, l, mid - 1);
         }
         return mid;
+    }
+
+    public int search2(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (r >= l) {
+            int mid = l + ((r - l) >>> 1);
+            int a = nums[mid];
+            if (target > a) {
+                l = mid + 1;
+            } else if (target < a) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 
     @Builder
