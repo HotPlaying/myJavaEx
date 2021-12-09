92c601cf9c59cd689a93c46f5d1efb3c3dc587b4
hotplaying <tangrd98@outlook.com>
2021-12-09 23:25:54 +0800

    leetcode: SearchRange#3

--- src/main/java/com/test/leetcode/dec/SearchRange.java
+++ src/main/java/com/test/leetcode/dec/SearchRange.java
@@ -20,6 +20,7 @@ public class SearchRange {
         System.out.println(ss(nums, 6));
         System.out.println(ss(nums, 10));
         System.out.println(ss(nums1, 0));
+        System.out.println(ss(new int[]{2, 2}, 3));
 
         System.out.println("预期结果：");
         System.out.println("[3, 4]");
@@ -27,12 +28,57 @@ public class SearchRange {
         System.out.println("[-1, -1]");
         System.out.println("[5, 5]");
         System.out.println("[-1, -1]");
+        System.out.println("[-1, -1]");
     }
 
     public String ss(int[] nums, int target) {
         return Arrays.toString(searchRange(nums, target));
     }
-    public int[] searchRange(int[] nums, int target) {
 
+    public int[] searchRange(int[] nums, int target) {
+        int r = rightBorder(nums, target);
+        int l = leftBorder(nums, target);
+        if (l == -2 || r == -2) {
+            return new int[]{-1, -1};
+        }
+        if (r - l > 1) {
+            return new int[]{l + 1, r - 1};
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public int rightBorder(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int rightBorder = -2;
+        while (l <= r) {
+            int mid = l + ((r - l) >>> 1);
+            int a = nums[mid];
+            if (target < a) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+                rightBorder = l;
+            }
+        }
+        return rightBorder;
+    }
+
+    public int leftBorder(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int leftBorder = -2;
+        while (l <= r) {
+            int mid = l + ((r - l) >>> 1);
+            int a = nums[mid];
+            if (target > a) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                leftBorder = r;
+            }
+        }
+        return leftBorder;
     }
 }
