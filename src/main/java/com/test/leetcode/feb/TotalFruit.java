e276e9bcf5cda8044503ab212fee5e2a77931284
trd <854397717@qq.com>
2022-02-28 17:59:46 +0800

    leetcode: TotalFruit#0

---
+++ src/main/java/com/test/leetcode/feb/TotalFruit.java
@@ -0,0 +1,12 @@
+package com.test.leetcode.feb;
+
+/**
+ * @author trd
+ * @since 2022/2/28 17:48
+ * https://leetcode-cn.com/problems/fruit-into-baskets/
+ */
+public class TotalFruit {
+    public int totalFruit(int[] fruits) {
+        return 0;
+    }
+}
